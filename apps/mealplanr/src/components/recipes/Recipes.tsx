6d660f1439eae8cb0be3362270f5298e5cb64af8
Andreas Petersen <andreasgdp@gmail.com>
2022-02-05 15:33:14 +0100

    Changed foldername of contact layer between frontend and backend
    + minor changes

--- apps/mealplanr/src/components/recipes/Recipes.tsx
+++ apps/mealplanr/src/components/recipes/Recipes.tsx
@@ -1,19 +1,19 @@
 import { useEffect, useState } from 'react';
-import { CategoryType } from '../../couplers/categories';
-import { getRecipe, getRecipes, getRecipesPlan, MealplanType, recipeDefaults, RecipesType } from '../../couplers/recipes';
+import { CategoryType } from '../../services/MP-API/categories';
+import { getRecipe, getRecipes, getRecipesPlan, MealplanType, recipeDefaults, RecipesType } from '../../services/MP-API/recipes';
 import './Recipes.scss';
 import altIMG from '/public/alt.png';
 
 // Handle to set placholder image when src is unavailable
 function handleAltImg(e: React.SyntheticEvent<HTMLImageElement, Event>) {
 	const target = e.target as HTMLImageElement;
-	const ptarget = target.parentElement as HTMLDivElement;
+	const pTarget = target.parentElement as HTMLDivElement;
 	target.src = altIMG;
-	ptarget.classList.remove("shadow"); // Shadow is applied to the path of svg - we do not want that
+	pTarget.classList.remove("shadow"); // Shadow is applied to the path of svg - we do not want that
 }
 
-// create Datasetcycleimg interface to include images in the html elements dataset list
-interface Datasetcycleimg extends HTMLImageElement {
+// create DataSetCycleImg interface to include images in the html elements dataset list
+interface DataSetCycleImg extends HTMLImageElement {
 	dataset: {
 		images: string;
 	};
@@ -21,14 +21,14 @@ interface Datasetcycleimg extends HTMLImageElement {
 
 // Cycle images
 function handleNextImage(e: React.SyntheticEvent<HTMLImageElement, Event>) {
-	const target = e.target as Datasetcycleimg;
-	const ptarget = target.parentElement as HTMLDivElement;
-	if(!ptarget.classList.contains("shadow")) return; // If no shadow, placeholder is used - no images at all
+	const target = e.target as DataSetCycleImg;
+	const pTarget = target.parentElement as HTMLDivElement;
+	if(!pTarget.classList.contains("shadow")) return; // If no shadow, placeholder is used - no images at all
 	const max = Number(target.dataset.images); // Data-tag containing amount of images assoatiated
 	const [id, current] = target.src.replace(/http:\/\/localhost:3000\/temp\/recipe_(\d+)_(\d+).jpg/,"$1,$2").split(","); // get current values
 	const next = (Number(current) + 1) % max; // Add one and loop in case of overflow
-	setTimeout(() => {target.src = "/temp/recipe_"+id+"_"+next+".jpg"}, 5000); // Wait 5s. Changing src will call onload causing chain reaction to trigger function again
-};
+	setTimeout(() => {target.src = `/temp/recipe_${id}_${next}.jpg`}, 5000); // Wait 5s. Changing src will call onload causing chain reaction to trigger function again
+}
 
 interface RecipeProps {
 	type: 'wide'|'tall',
@@ -55,7 +55,7 @@ export function Recipe(props: RecipeProps) {
 	<>
 		<div className="rimage shadow">
 			{	personal ? <span className={"options"}></span> : null }
-			<img src={`data:image/jpeg;base64,${images[0]}`} onError={handleAltImg} alt="" onLoad={handleNextImage}></img>
+			<img src={`data:image/jpeg;base64,${images[0]}`} onError={handleAltImg} alt="" onLoad={handleNextImage} />
 		</div>
 		<h3>{title}</h3>
 		<p>{description}</p>
@@ -71,14 +71,14 @@ export function Recipe(props: RecipeProps) {
 				</div>
 				<div className="ratingbox">{rating[0]} votes</div>
 				<div>
-					<span className="rating icon" style={{width: (rating[0]/rating[1]/5*100)+"%"}}></span>
+					<span className="rating icon" style={{width: `${rating[0]/rating[1]/5*100}%`}} />
 				</div>
 			</>
 		}
 	</>;
 
 	return (
-		<div className={type + ' recipe ' + (categoriesData.length > 0 ? categoriesData.join(" ") : 'empty')} id={recipeId+''} onClick={() => console.log('Clicked recipe')}>
+		<div className={`${type} recipe ${categoriesData.length > 0 ? categoriesData.join(" ") : 'empty'}`} id={recipeId} onClick={() => console.log('Clicked recipe')}>
 			{ body ??
 				<>
 					<h3>Add recipe</h3>
@@ -105,8 +105,8 @@ export default function Recipes(props: RecipesProps) {
 	if(mealFrom === 'plan')
 		return (
 			<>
-				{getRecipesPlan().map((recipesitem: MealplanType, index: number) => (
-					<Recipe key={index} type={'tall'} recipeId={recipesitem.recipeId} At={recipesitem.time} personal={true} />
+				{getRecipesPlan().map((recipesItem: MealplanType, index: number) => (
+					<Recipe key={index} type={'tall'} recipeId={recipesItem.recipeId} At={recipesItem.time} personal={true} />
 				))}
 			</>
 		);
@@ -120,8 +120,8 @@ export default function Recipes(props: RecipesProps) {
 						<p>+</p>
 					</div>
 				) : ''}
-				{listData.map((recipesitem: RecipesType, index: number) => 
-					<Recipe key={index} type={'wide'} recipeId={recipesitem.recipeId} personal={mealFrom==='personal'} />
+				{listData.map((recipesItem: RecipesType, index: number) => 
+					<Recipe key={index} type={'wide'} recipeId={recipesItem.recipeId} personal={mealFrom==='personal'} />
 				)}
 			</>
 		);
