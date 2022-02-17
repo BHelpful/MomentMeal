91b6285f3e192575e5bf7f143b63d2da1f610d6d
Andreasgdp <andreasgdp@gmail.com>
2022-02-17 15:42:11 +0100

    Complete working store route

--- apps/mealplanr-api/src/collections/store/store.service.ts
+++ apps/mealplanr-api/src/collections/store/store.service.ts
@@ -5,27 +5,48 @@ import {
 	QueryOptions,
 } from 'mongoose';
 import storeModel, {
-	Store,
 	StoreCreationParams,
 	StoreDocument,
 } from './store.model';
 import sanitize = require('mongo-sanitize');
-import log from '../../logger';
 
 export class StoresService {
+	public create(body: StoreCreationParams): Promise<StoreDocument> {
+		return this.createStore(body);
+	}
+
+	public get(_id: string): Promise<StoreDocument | null> {
+		return this.findStore({ _id });
+	}
+
+	public update(
+		query: FilterQuery<StoreDocument>,
+		update: UpdateQuery<StoreDocument>
+	) {
+		return this.findAndUpdateStore(query, update, {
+			new: true,
+			// This is false because setting it true deprecated https://mongoosejs.com/docs/deprecations.html#findandmodify
+			useFindAndModify: false,
+		});
+	}
+
+	public delete(_id: string): Promise<StoreDocument | null> {
+		return this.deleteStore({ _id });
+	}
+
 	/**
 	 * This function will create a new store for a user and return the store
 	 *
 	 * @param body - The body of the store (based on the storeModel)
 	 * @returns a store document
 	 */
-	public create(body: StoreCreationParams): Promise<StoreDocument> {
-		body = sanitize(body);
-		return storeModel.create(body);
-	}
-
-	public get(_id: string): Promise<StoreDocument | null> {
-		return this.findStore({ _id });
+	private async createStore(body: DocumentDefinition<StoreDocument>) {
+		try {
+			body = sanitize(body);
+			return await storeModel.create(body);
+		} catch (error) {
+			throw new Error(error as string);
+		}
 	}
 
 	/**
@@ -46,41 +67,41 @@ export class StoresService {
 			throw new Error(error as string);
 		}
 	}
-}
 
-/**
- * This function will find, update and return a store
- *
- * @param query - a query object that will be used to find a store from the DB
- * @param update - a query object that will be used to specify the update
- * @param options - options for the findOne function from mongoose
- * @returns a store document
- */
-export async function findAndUpdateStore(
-	query: FilterQuery<StoreDocument>,
-	update: UpdateQuery<StoreDocument>,
-	options: QueryOptions
-) {
-	try {
-		query = sanitize(query);
-		update = sanitize(update);
-		return storeModel.findOneAndUpdate(query, update, options);
-	} catch (error) {
-		throw new Error(error as string);
+	/**
+	 * This function will find, update and return a store
+	 *
+	 * @param query - a query object that will be used to find a store from the DB
+	 * @param update - a query object that will be used to specify the update
+	 * @param options - options for the findOne function from mongoose
+	 * @returns a store document
+	 */
+	private async findAndUpdateStore(
+		query: FilterQuery<StoreDocument>,
+		update: UpdateQuery<StoreDocument>,
+		options: QueryOptions
+	) {
+		try {
+			query = sanitize(query);
+			update = sanitize(update);
+			return storeModel.findOneAndUpdate(query, update, options);
+		} catch (error) {
+			throw new Error(error as string);
+		}
 	}
-}
 
-/**
- * This function will find and delete a store
- *
- * @param query - a query object that will be used to find a store from the DB
- * @returns a store document
- */
-export async function deleteStore(query: FilterQuery<StoreDocument>) {
-	try {
-		query = sanitize(query);
-		return storeModel.deleteOne(query);
-	} catch (error) {
-		throw new Error(error as string);
+	/**
+	 * This function will find and delete a store
+	 *
+	 * @param query - a query object that will be used to find a store from the DB
+	 * @returns a store document
+	 */
+	private async deleteStore(query: FilterQuery<StoreDocument>) {
+		try {
+			query = sanitize(query);
+			return storeModel.deleteOne(query);
+		} catch (error) {
+			throw new Error(error as string);
+		}
 	}
 }
