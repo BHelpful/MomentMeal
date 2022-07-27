69de55d4ed78264954ec651cbf7f1ceb0ed02b7b
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2022-07-27 17:04:54 +0200

    Fix code smell

--- apps/meal-time-e2e/src/integration/app.spec.ts
+++ apps/meal-time-e2e/src/integration/app.spec.ts
@@ -1,13 +1,7 @@
-import { getGreeting } from '../support/app.po';
-
 describe('meal-time', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    // cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    // getGreeting().contains('Welcome meal-time');
+  it('should pass', () => {
+    expect(true).to.equal(true);
   });
 });
