1e70f9083e2b6d7c61848b5a35785247ed113b25
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2022-07-27 06:45:27 +0000

    Completed .env generator

--- tools/generators/gen-env/index.ts
+++ tools/generators/gen-env/index.ts
@@ -1,8 +1,36 @@
-import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
-import { libraryGenerator } from '@nrwl/workspace/generators';
+import {
+  Tree,
+  formatFiles,
+  installPackagesTask,
+  generateFiles,
+  joinPathFragments,
+} from '@nrwl/devkit';
 
-export default async function (tree: Tree, schema: any) {
-  await libraryGenerator(tree, { name: schema.name });
+interface NewArticleSchemaOptions {
+  db_uri: string;
+  test_db_uri: string;
+}
+
+export default async function (tree: Tree, schema: NewArticleSchemaOptions) {
+  generateFiles(
+    // virtual file system
+    tree,
+
+    // the location where the template files are
+    joinPathFragments(__dirname, './files'),
+
+    // where the files should be generated
+    './',
+
+    // the variables to be substituted in the template
+    {
+      db_uri: schema.db_uri,
+      test_db_uri: schema.test_db_uri,
+
+      template: '',
+    }
+  );
+
   await formatFiles(tree);
   return () => {
     installPackagesTask(tree);
