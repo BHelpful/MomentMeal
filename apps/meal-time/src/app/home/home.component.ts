82c363442997405c1f26372ca29ab6fb59a41b42
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2022-08-01 12:50:07 +0000

    remove OnInit from home component

--- apps/meal-time/src/app/home/home.component.ts
+++ apps/meal-time/src/app/home/home.component.ts
@@ -1,15 +1,12 @@
 import ThirdPartyEmailPassword from 'supertokens-web-js/recipe/thirdpartyemailpassword';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'meal-time-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  ngOnInit(): void {
-    console.log('HomeComponent.ngOnInit');
-  }
+export class HomeComponent {
 
   async signOut() {
     await ThirdPartyEmailPassword.signOut();
