499583e0730b0b87ccf6a6968f0c35938e634964
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2022-08-31 14:34:25 +0200

    Implemented correct disabled state of mt-button

--- libs/mealtime-ui-components/src/lib/button/button.component.ts
+++ libs/mealtime-ui-components/src/lib/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 export type ButtonVariant = 'primary' | 'secondary' | 'tertiary' | 'link';
 
@@ -16,4 +16,13 @@ export class ButtonComponent {
 
   @Input()
   public value = 'Button';
+
+  @Output()
+  public clicked = new EventEmitter();
+
+  public onClick() {
+    if (!this.disabled) {
+      this.clicked.emit();
+    }
+  }
 }
