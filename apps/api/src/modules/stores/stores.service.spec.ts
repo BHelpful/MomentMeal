80ca61d7d8239599000f77a6e3e8cbc1e65fef02
Max Hansen (Maxx0758) <maxx0758@gmail.com>
2022-09-02 16:02:34 +0200

    Add unit tests for stores service

--- apps/api/src/modules/stores/stores.service.spec.ts
+++ apps/api/src/modules/stores/stores.service.spec.ts
@@ -1,20 +1,113 @@
+import { CreateStoreDto, UpdateStoreDto } from '@meal-time/api-interfaces';
+import { ServiceUnavailableException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Stores } from '@prisma/client';
 import { PrismaService } from '../../services/prisma/prisma.service';
 import { StoresService } from './stores.service';
 
 describe('StoresService', () => {
-  let service: StoresService;
+	let service: StoresService;
+	let prisma: PrismaService;
+	let store1: Stores;
+	let store2: Stores;
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [StoresService, PrismaService, ConfigService],
+		}).compile();
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [StoresService, PrismaService, ConfigService],
-    }).compile();
+		service = module.get<StoresService>(StoresService);
+		prisma = module.get<PrismaService>(PrismaService);
+		await prisma.cleanDb();
 
-    service = module.get<StoresService>(StoresService);
-  });
+		const dto: CreateStoreDto = {
+			name: 'Brugsen',
+		};
+		const dto1: CreateStoreDto = {
+			name: 'Fakta',
+		};
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+		store1 = await prisma.stores.create({
+			data: {
+				...dto,
+			},
+		});
+		store2 = await prisma.stores.create({
+			data: {
+				...dto1,
+			},
+		});
+	});
+
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
+
+	it('should create store', async () => {
+		const dto: CreateStoreDto = {
+			name: 'Meny',
+		};
+		const store = await service.create(dto);
+		return expect(store).toHaveProperty('name');
+	});
+
+	it('should find all stores', async () => {
+		const prismaStores = await prisma.stores.findMany();
+		prismaStores.sort((a, b) => {
+			if (a.id > b.id) {
+				return 1;
+			} else if (a.id < b.id) {
+				return -1;
+			}
+			return 0;
+		});
+
+		const stores = await service.findAll();
+
+		return expect(stores).toEqual(prismaStores);
+	});
+
+	it('should find specific store', async () => {
+		const foundStore = await service.findOne(store1.id);
+
+		return expect(foundStore).toEqual(store1);
+	});
+
+	it('should update store', async () => {
+		const udto: UpdateStoreDto = {
+			name: 'Primark',
+		};
+		const updateStore = await service.update(store1.id, udto);
+		if (updateStore) {
+			return expect(updateStore.name).toEqual('Primark');
+		}
+		return false;
+	});
+
+	it('should remove store', async () => {
+		const stores = await prisma.stores.findMany();
+		stores.sort((a, b) => {
+			if (a.id > b.id) {
+				return 1;
+			} else if (a.id < b.id) {
+				return -1;
+			}
+			return 0;
+		});
+
+		const storeToRemove = stores.pop();
+		await service.remove(storeToRemove.id);
+
+		const storesAfterRemove = await prisma.stores.findMany();
+		storesAfterRemove.sort((a, b) => {
+			if (a.id > b.id) {
+				return 1;
+			} else if (a.id < b.id) {
+				return -1;
+			}
+			return 0;
+		});
+
+		return expect(storesAfterRemove).toEqual(stores);
+	});
 });
