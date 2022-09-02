f8264513ef74637c76bcc25e54b91facf5049684
Max Hansen (Maxx0758) <maxx0758@gmail.com>
2022-09-02 20:04:02 +0200

    Add edge cases tests to stores services

--- apps/api/src/modules/stores/stores.service.spec.ts
+++ apps/api/src/modules/stores/stores.service.spec.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from '../../services/prisma/prisma.service';
 import { StoresService } from './stores.service';
+import { STORES_EXCEPTION_MSG } from './stores.exceptionMessages';
 
 describe('StoresService', () => {
 	let module: TestingModule;
@@ -28,6 +29,20 @@ describe('StoresService', () => {
 
 		const store = await service.create(dto);
 		return expect(store.name).toEqual('Meny');
+	});
+
+	it('should not create store that already exists', async () => {
+		const dto: CreateStoreDto = {
+			name: 'Meny',
+		};
+		const dto2: CreateStoreDto = {
+			name: 'Meny',
+		};
+		await service.create(dto);
+
+		return expect(async () => {
+			await service.create(dto2);
+		}).rejects.toThrow(STORES_EXCEPTION_MSG.ALREADY_EXISTS);
 	});
 
 	it('should find all stores', async () => {
@@ -58,6 +73,12 @@ describe('StoresService', () => {
 		return expect(foundStore).toEqual(store);
 	});
 
+	it('should not find store that does not exist', async () => {
+		return expect(async () => {
+			await service.findOne(10000);
+		}).rejects.toThrow(STORES_EXCEPTION_MSG.NOT_FOUND);
+	});
+
 	it('should update store', async () => {
 		const dto: CreateStoreDto = {
 			name: 'Brugsen',
@@ -71,6 +92,35 @@ describe('StoresService', () => {
 
 		const updatedStore = await service.update(store.id, udto);
 		return expect(updatedStore.name).toEqual('Primark');
+	});
+
+	it('should not update store to store that already exists', async () => {
+		const dto: CreateStoreDto = {
+			name: 'Brugsen',
+		};
+		const dto2: CreateStoreDto = {
+			name: 'Fakta',
+		};
+
+		const store = await service.create(dto);
+		await service.create(dto2);
+
+		const udto: UpdateStoreDto = {
+			name: 'Fakta',
+		};
+
+		return expect(async () => {
+			await service.update(store.id, udto);
+		}).rejects.toThrow(STORES_EXCEPTION_MSG.ALREADY_EXISTS);
+	});
+
+	it('should not update store that does not exist', async () => {
+		const udto: UpdateStoreDto = {
+			name: 'Fakta',
+		};
+		return expect(async () => {
+			await service.update(10000, udto);
+		}).rejects.toThrow(STORES_EXCEPTION_MSG.NOT_FOUND);
 	});
 
 	it('should remove store', async () => {
@@ -91,5 +141,11 @@ describe('StoresService', () => {
 
 		const storesAfterRemove = await service.findAll();
 		return expect(storesAfterRemove).toEqual(stores);
+	});
+
+	it('should not remove store that does not exist', async () => {
+		return expect(async () => {
+			await service.remove(10000);
+		}).rejects.toThrow(STORES_EXCEPTION_MSG.NOT_FOUND);
 	});
 });
