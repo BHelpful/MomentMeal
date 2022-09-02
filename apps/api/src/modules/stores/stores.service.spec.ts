3bec159c32f7d784b489b91154e87170be47f5b6
Max Hansen (Maxx0758) <maxx0758@gmail.com>
2022-09-03 00:36:33 +0200

    Make db clean before contoller tests

--- apps/api/src/modules/stores/stores.service.spec.ts
+++ apps/api/src/modules/stores/stores.service.spec.ts
@@ -16,6 +16,7 @@ describe('StoresService', () => {
 
 		service = module.get<StoresService>(StoresService);
 		prisma = module.get<PrismaService>(PrismaService);
+		await prisma.cleanDb();
 	});
 
 	beforeEach(async () => {
