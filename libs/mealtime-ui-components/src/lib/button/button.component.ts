813c3908cf7129ab212f0792de552e8b552e2789
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2022-09-12 09:31:07 +0200

    Enabled btn with icons and custom color

--- libs/mealtime-ui-components/src/lib/button/button.component.ts
+++ libs/mealtime-ui-components/src/lib/button/button.component.ts
@@ -1,28 +1,61 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 export type ButtonVariant = 'primary' | 'secondary' | 'tertiary' | 'link';
 
 @Component({
-  selector: 'mt-button',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss'],
+	selector: 'mt-button',
+	templateUrl: './button.component.html',
+	styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent {
+	@Input()
+	public disabled = false;
+
+	@Input()
+	public variant: ButtonVariant = 'primary';
+
+	@Input()
+	public bgColor = '';
+	
   @Input()
-  public disabled = false;
+	public color = '';
 
-  @Input()
-  public variant: ButtonVariant = 'primary';
+	@Input()
+	public value = 'Button';
 
-  @Input()
-  public value = 'Button';
+	@Input()
+	public iconPath: string | IconDefinition = '';
 
-  @Output()
-  public clicked = new EventEmitter();
+	@Output()
+	public clicked = new EventEmitter();
 
-  public onClick() {
-    if (!this.disabled) {
-      this.clicked.emit();
-    }
-  }
+	public onClick() {
+		if (!this.disabled) {
+			this.clicked.emit();
+		}
+	}
+
+	getStringIcon(): string {
+		return this.iconPath as string;
+	}
+
+	getFaIcon(): IconDefinition {
+		return this.iconPath as IconDefinition;
+	}
+
+	useStringIcon(): boolean {
+		return (
+			typeof this.iconPath === 'string' &&
+			this.iconPath.length >= 1 &&
+			this.iconPath !== undefined
+		);
+	}
+	useFaIcon(): boolean {
+		return (
+			typeof this.iconPath !== 'string' &&
+			'icon' in this.iconPath &&
+			this.iconPath !== undefined
+		);
+	}
 }
