35678dfebcdce1dd36a41376e712de5802da85c2
Max Hansen (Maxx0758) <maxx0758@gmail.com>
2022-10-01 02:06:10 +0200

    Add categories model

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,31 +5,40 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  // create two dummy stores
-  const store1 = await prisma.stores.upsert({
-    where: { name: 'Rema 1000' },
-    update: {},
-    create: { name: 'Rema 1000' },
-  });
+	// create two dummy stores
+	const store1 = await prisma.stores.upsert({
+		where: { name: 'Rema 1000' },
+		update: {},
+		create: { name: 'Rema 1000' },
+	});
 
-  const store2 = await prisma.stores.upsert({
-    where: { name: 'Lidl' },
-    update: {},
-    create: {
-      name: 'Lidl',
-    },
-  });
+	const store2 = await prisma.stores.upsert({
+		where: { name: 'Lidl' },
+		update: {},
+		create: {
+			name: 'Lidl',
+		},
+	});
 
-  console.log({ post1: store1, post2: store2 });
+	console.log({ post1: store1, post2: store2 });
+
+	const category1 = await prisma.categories.upsert({
+		where: { name: 'Mexican' },
+		update: {},
+		create: {
+			name: 'Mexican',
+		},
+	});
+	console.log({ post1: category1 });
 }
 
 // execute the main function
 main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    // close Prisma Client at the end
-    await prisma.$disconnect();
-  });
+	.catch((e) => {
+		console.error(e);
+		process.exit(1);
+	})
+	.finally(async () => {
+		// close Prisma Client at the end
+		await prisma.$disconnect();
+	});
