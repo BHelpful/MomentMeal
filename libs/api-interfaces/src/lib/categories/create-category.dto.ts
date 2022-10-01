ef87bde4e35a67c133ee22c0dfe5c12693eb984f
Max Hansen (Maxx0758) <maxx0758@gmail.com>
2022-10-01 02:05:37 +0200

    Add create catecory DTO

--- libs/api-interfaces/src/lib/categories/create-category.dto.ts
+++ libs/api-interfaces/src/lib/categories/create-category.dto.ts
@@ -1,1 +1,22 @@
-export class CreateCategoryDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsNotEmpty, IsString, ArrayNotEmpty } from 'class-validator';
+
+export class CreateCategoryDto {
+	@IsNotEmpty()
+	@IsString()
+	@ApiProperty({
+		description: 'Name of a category',
+		default: 'Mexican',
+	})
+	readonly name: string;
+
+	@IsNotEmpty()
+	@IsArray()
+	@ArrayNotEmpty()
+	@IsString({ each: true }) //Runs on every item in the array
+	@ApiProperty({
+		description: "The category's types",
+		default: ['spicy', 'liquid'],
+	})
+	readonly type: string[];
+}
