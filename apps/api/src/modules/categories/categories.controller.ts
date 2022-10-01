41d3941bde073bad865f760a6b3879a1a3ebb0c9
Max Hansen (Maxx0758) <maxx0758@gmail.com>
2022-10-01 02:04:26 +0200

    Add API responses and descriptions to api operations on categories

--- apps/api/src/modules/categories/categories.controller.ts
+++ apps/api/src/modules/categories/categories.controller.ts
@@ -1,46 +1,126 @@
-import { CreateCategoryDto, UpdateCategoryDto } from '@meal-time/api-interfaces';
 import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
+	CreateCategoryDto,
+	UpdateCategoryDto,
+	CategoriesEntity,
+} from '@meal-time/api-interfaces';
+import {
+	Controller,
+	Get,
+	Post,
+	Body,
+	Patch,
+	Param,
+	Delete,
+	ForbiddenException,
+	InternalServerErrorException,
+	NotFoundException,
+	BadRequestException,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+	ApiTags,
+	ApiOperation,
+	ApiCreatedResponse,
+	ApiResponse,
+	ApiOkResponse,
+} from '@nestjs/swagger';
 import { CategoriesService } from './categories.service';
+import { CATEGORIES_EXCEPTION_MSG } from './categories.exceptionMessages';
 
 @ApiTags('Categories')
 @Controller('categories')
 export class CategoriesController {
-  constructor(private readonly categoriesService: CategoriesService) {}
+	constructor(private readonly categoriesService: CategoriesService) {}
 
-  @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
-    return this.categoriesService.create(createCategoryDto);
-  }
+	@ApiOperation({
+		summary: 'Create a new category',
+		description: 'Create a new category',
+	})
+	@ApiCreatedResponse({ type: CategoriesEntity })
+	@ApiResponse({
+		status: new ForbiddenException().getStatus(),
+		description: CATEGORIES_EXCEPTION_MSG.ALREADY_EXISTS,
+	})
+	@ApiResponse({
+		status: new InternalServerErrorException().getStatus(),
+		description: CATEGORIES_EXCEPTION_MSG.INTERNAL_SERVER_ERROR,
+	})
+	@Post()
+	create(@Body() createCategoryDto: CreateCategoryDto) {
+		return this.categoriesService.create(createCategoryDto);
+	}
 
-  @Get()
-  findAll() {
-    return this.categoriesService.findAll();
-  }
+	@ApiOperation({
+		summary: 'Get all categories',
+		description: 'Get all categories',
+	})
+	@ApiOkResponse({ type: CategoriesEntity })
+	@Get()
+	findAll() {
+		return this.categoriesService.findAll();
+	}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.categoriesService.findOne(+id);
-  }
+	@ApiOperation({
+		summary: 'Find a category by id',
+		description: 'Find a category by a specific id',
+	})
+	@ApiOkResponse({ type: CategoriesEntity })
+	@ApiResponse({
+		status: new NotFoundException().getStatus(),
+		description: CATEGORIES_EXCEPTION_MSG.NOT_FOUND,
+	})
+	@ApiResponse({
+		status: new BadRequestException().getStatus(),
+		description: CATEGORIES_EXCEPTION_MSG.BAD_REQUEST,
+	})
+	@Get(':id')
+	findOne(@Param('id') id: string) {
+		return this.categoriesService.findOne(+id);
+	}
 
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateCategoryDto: UpdateCategoryDto
-  ) {
-    return this.categoriesService.update(+id, updateCategoryDto);
-  }
+	@ApiOperation({
+		summary: 'Update a category by id',
+		description: 'Update a category by a specific id',
+	})
+	@ApiOkResponse({ type: CategoriesEntity })
+	@ApiResponse({
+		status: new NotFoundException().getStatus(),
+		description: CATEGORIES_EXCEPTION_MSG.NOT_FOUND,
+	})
+	@ApiResponse({
+		status: new ForbiddenException().getStatus(),
+		description: CATEGORIES_EXCEPTION_MSG.ALREADY_EXISTS,
+	})
+	@ApiResponse({
+		status: new BadRequestException().getStatus(),
+		description: CATEGORIES_EXCEPTION_MSG.BAD_REQUEST,
+	})
+	@ApiResponse({
+		status: new InternalServerErrorException().getStatus(),
+		description: CATEGORIES_EXCEPTION_MSG.INTERNAL_SERVER_ERROR,
+	})
+	@Patch(':id')
+	update(
+		@Param('id') id: string,
+		@Body() updateCategoryDto: UpdateCategoryDto
+	) {
+		return this.categoriesService.update(+id, updateCategoryDto);
+	}
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.categoriesService.remove(+id);
-  }
+	@ApiOperation({
+		summary: 'Delete a category by id',
+		description: 'Delete a category by a specific id',
+	})
+	@ApiOkResponse({ type: CategoriesEntity })
+	@ApiResponse({
+		status: new NotFoundException().getStatus(),
+		description: CATEGORIES_EXCEPTION_MSG.NOT_FOUND,
+	})
+	@ApiResponse({
+		status: new BadRequestException().getStatus(),
+		description: CATEGORIES_EXCEPTION_MSG.BAD_REQUEST,
+	})
+	@Delete(':id')
+	remove(@Param('id') id: string) {
+		return this.categoriesService.remove(+id);
+	}
 }
