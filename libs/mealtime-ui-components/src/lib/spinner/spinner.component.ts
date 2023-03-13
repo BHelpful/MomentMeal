4e60e468a011c6ecf5c4b56b5daf9845ff63d9ea
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2023-03-13 20:17:29 +0100

    Address sonar code smell

--- libs/mealtime-ui-components/src/lib/spinner/spinner.component.ts
+++ libs/mealtime-ui-components/src/lib/spinner/spinner.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'mt-spinner',
 	templateUrl: './spinner.component.html',
-	styleUrls: ['./spinner.component.scss'],
 })
 export class SpinnerComponent {}
