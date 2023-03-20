641af931e4c141863c0068c56fbb1626d377e22a
Max Hansen <maxx0758@gmail.com>
2023-03-20 14:23:56 +0100

    Start on some back-end I guess
    Current in class so W.I.P

--- apps/api/src/modules/categories/categories.service.ts
+++ apps/api/src/modules/categories/categories.service.ts
@@ -1,25 +1,76 @@
-import { CreateCategoryDto, UpdateCategoryDto } from '@meal-time/api-interfaces';
-import { Injectable } from '@nestjs/common';
-
+import {
+	CreateCategoryDto,
+	UpdateCategoryDto,
+} from '@meal-time/api-interfaces';
+import {
+	BadRequestException,
+	Injectable,
+	NotFoundException,
+} from '@nestjs/common';
+import { Categories, Prisma } from '@prisma/client';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { PrismaService } from '../../services/prisma/prisma.service';
+import { ExceptionMessages } from '../../utils/exceptionMessages';
+import { prismaErrorException } from '../../utils/prismaErrorHandler';
 @Injectable()
 export class CategoriesService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
-  }
+	public categoriesExeptionMessages = new ExceptionMessages('Category');
+	constructor(private prisma: PrismaService) {}
 
-  findAll() {
-    return `This action returns all categories`;
-  }
+	async create(createCategoryDto: CreateCategoryDto) {
+		return this.prisma.categories
+			.create({
+				data: {
+					...createCategoryDto,
+				},
+			})
+			.catch((error: PrismaClientKnownRequestError) => {
+				throw prismaErrorException(error, this.categoriesExeptionMessages);
+			});
+	}
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
-  }
+	async findAll() {
+		return this.prisma.categories
+			.findMany({
+				orderBy: {
+					id: 'asc',
+				},
+			})
+			.catch((error: PrismaClientKnownRequestError) => {
+				throw prismaErrorException(error, this.categoriesExeptionMessages);
+			});
+	}
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
-  }
+	async findOne(id: number) {
+		if (isNaN(id)) {
+			throw new BadRequestException(
+				this.categoriesExeptionMessages.BAD_REQUEST()
+			);
+		}
+		const category = await this.findCategory(id);
+		if (!category) {
+			throw new NotFoundException(this.categoriesExeptionMessages.NOT_FOUND());
+		}
+		return this.findCategory(id);
+	}
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
-  }
+	private async findCategory(id: number) {
+		return this.prisma.categories
+			.findUnique({
+				where: {
+					id: id,
+				},
+			})
+			.catch((error: PrismaClientKnownRequestError) => {
+				throw prismaErrorException(error, this.categoriesExeptionMessages);
+			});
+	}
+
+	async update(id: number, updateCategoryDto: UpdateCategoryDto) {
+		return `This action updates a #${id} category`;
+	}
+
+	async remove(id: number) {
+		return `This action removes a #${id} category`;
+	}
 }
