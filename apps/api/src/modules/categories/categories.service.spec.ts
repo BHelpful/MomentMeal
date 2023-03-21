0c915a8d69ea77512482303a0a28cfe6cd42f3b5
Max Hansen <maxx0758@gmail.com>
2023-03-21 13:15:49 +0100

    Start on categories service test

--- apps/api/src/modules/categories/categories.service.spec.ts
+++ apps/api/src/modules/categories/categories.service.spec.ts
@@ -1,18 +1,33 @@
+import { CreateCategoryDto } from '@meal-time/api-interfaces';
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../../services/prisma/prisma.service';
 import { CategoriesService } from './categories.service';
 
 describe('CategoriesService', () => {
-  let service: CategoriesService;
+	let service: CategoriesService;
+	let prisma: PrismaService;
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [CategoriesService, PrismaService, ConfigService],
+		}).compile();
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoriesService],
-    }).compile();
+		service = module.get<CategoriesService>(CategoriesService);
+		prisma = module.get<PrismaService>(PrismaService);
+		await prisma.cleanDb();
+	});
 
-    service = module.get<CategoriesService>(CategoriesService);
-  });
+	beforeEach(async () => {
+		await prisma.cleanDb();
+	});
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+	it('should create category', async () => {
+		const dto: CreateCategoryDto = {
+			name: 'Paprika',
+			type: ['Spicy', 'Powder'],
+		};
+
+		const category = await service.create(dto);
+		return expect(category.name).toEqual('Paprika');
+	});
 });
