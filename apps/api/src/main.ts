5ed81f142b117583451764c0f478d8d52cdb97d3
Andreas Petersen <andreasgdp@gmail.com>
2023-05-06 22:32:03 +0200

    Add custom js to nestjs swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -40,6 +40,10 @@ async function bootstrap() {
 	SwaggerModule.setup('api', app, document, {
 		customCssUrl:
 			'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+		customJs: [
+			'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+			'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+		],
 	});
 
 	await app.listen(port);
