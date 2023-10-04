ff3dd7e5f7454f29a6a9f1b3b4bc51bc7611c192
Andreas Petersen <andreasgdp@gmail.com>
2023-10-04 21:04:52 +0200

    Another try at eliminating theme flicker on initial load

--- src/components/layouts/theme-toggle.tsx
+++ src/components/layouts/theme-toggle.tsx
@@ -1,18 +1,17 @@
-"use client"
+'use client';
 
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
-import { useTheme } from "next-themes"
-
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button';
+import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
+import { useTheme } from 'next-themes';
 
 export function ThemeToggle() {
-  const { setTheme, theme } = useTheme()
+  const { setTheme, theme } = useTheme();
 
   return (
     <Button
       variant="ghost"
       size="icon"
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
     >
       <SunIcon
         className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
@@ -24,5 +23,5 @@ export function ThemeToggle() {
       />
       <span className="sr-only">Toggle theme</span>
     </Button>
-  )
+  );
 }
--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -10,26 +10,26 @@ import { type ThemeProviderProps } from 'next-themes/dist/types';
 import { useState } from 'react';
 
 const Providers = ({ children, ...props }: ThemeProviderProps) => {
-    const [queryClient] = useState(() => new QueryClient());
-    const [trpcClient] = useState(() =>
-        trpc.createClient({
-            links: [
-                httpBatchLink({
-                    url: absoluteUrl('/api/trpc'),
-                }),
-            ],
-        })
-    );
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: absoluteUrl('/api/trpc'),
+        }),
+      ],
+    })
+  );
 
-    return (
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
         <NextThemesProvider {...props}>
-            <trpc.Provider client={trpcClient} queryClient={queryClient}>
-                <QueryClientProvider client={queryClient}>
-                    <TooltipProvider>{children}</TooltipProvider>
-                </QueryClientProvider>
-            </trpc.Provider>
+          <TooltipProvider>{children}</TooltipProvider>
         </NextThemesProvider>
-    );
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
 };
 
 export default Providers;
