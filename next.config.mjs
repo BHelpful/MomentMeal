da79c6b8c81c5e6cb49c20481d25ae72a5ae106d
Andreas Petersen <andreasgdp@gmail.com>
2023-10-14 11:20:56 +0200

    Rempve config properties from pwa config

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ import withPWA from 'next-pwa';
 
 const pwaConfig = {
   dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
 };
 
 const withPwaConfig = withPWA(pwaConfig);
