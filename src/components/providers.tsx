2ad56205c4f0ec88967f70d3c556a7825556ead2
Andreas Petersen <andreasgdp@gmail.com>
2024-05-06 21:52:10 +0200

    Add vercel speed insights

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,6 +2,7 @@
 
 import TrpcProvider from '@/app/_trpc/provider';
 import { TooltipProvider } from '@/components/ui/tooltip';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { type ThemeProviderProps } from 'next-themes/dist/types';
 
@@ -9,6 +10,7 @@ const Providers = ({ children, ...props }: ThemeProviderProps) => {
   return (
     <TrpcProvider>
       <NextThemesProvider {...props}>
+        <SpeedInsights />
         <TooltipProvider>{children}</TooltipProvider>
       </NextThemesProvider>
     </TrpcProvider>
