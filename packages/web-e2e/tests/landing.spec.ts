6b361f8a535ddefbb088d9d0bf06dc8b9befd516
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2024-08-05 22:17:42 +0200

    feat: add playwright e2e setup (#608)
    format, build, e2e setup missing linting and missing a database to run against for more complex e2e.
    
    This means in order to do more real e2e we need to at a later point add a dockerized postgres database (preferably using the docker-compose.yml file in the root). We will also need to setup clerk for testing https://clerk.com/docs/testing/playwright.
    
    fixes #607

---
+++ packages/web-e2e/tests/landing.spec.ts
@@ -0,0 +1,9 @@
+// landing.spec.ts
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/MomentMeal/);
+});
